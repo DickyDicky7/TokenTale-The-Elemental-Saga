35de6aced306545608a405d8f61f8613e8ec26d2
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2024-05-25 22:15:41 +0700

    add VisibleOnScreenEnabler3D for monsters

--- game_objects/characters/shared/Character3D.cs
+++ game_objects/characters/shared/Character3D.cs
@@ -5,16 +5,16 @@ namespace TokenTaleTheElementalSaga;
 [GlobalClass]
 public abstract partial class Character3D : CharacterBody3D
 {
-    public float CurrentHealth{ get; set; }
-    public float MaxHealth { get; set; } = 1.0f;
-    public float CurrentSpeed { get; set; }
-    public float Speed { get; set; } = 1.0f;
-    public float Jumpv { get; set; } = 1.0f;
-    public float Acceleration { get; set; } = 0.1f;
-    public float Deceleration { get; set; } = 0.1f;
-	public Timer EffectTimer { get; private set; } = new();
+    public float CurrentHealth { get; set; }
+    public float     MaxHealth { get; set; } = 1.0f;
+    public float CurrentSpeed  { get; set; }
+    public float        Speed  { get; set; } = 1.0f;
+    public float        Jumpv  { get; set; } = 1.0f;
+    public float Acceleration  { get; set; } = 0.1f;
+    public float Deceleration  { get; set; } = 0.1f;
+	public Timer EffectTimer   { get; private set; } = new();
 	[Signal]
-    public delegate void HealthChangeEventHandler(float Damage);
+    public delegate void HealthChangeEventHandler(float damage);
 
     public virtual void Move(Vector3 @direction, double @delta)
     {
@@ -77,11 +77,13 @@ public abstract partial class Character3D : CharacterBody3D
     public override void _Ready()
     {
                     base._Ready();
-		this.EffectTimer.OneShot = true;
-		this.EffectTimer.WaitTime = 1.0f;
+
+		this.EffectTimer. OneShot = true;
+		this.EffectTimer.WaitTime = 1.0d;
 		this.EffectTimer.ProcessCallback = Timer.TimerProcessCallback.Physics;
-        this.EffectTimer.ProcessMode = ProcessModeEnum.Always;
-		this.AddChild(EffectTimer);
+        this.EffectTimer.ProcessMode     =            ProcessModeEnum. Always;
+		this.AddChild   (
+             EffectTimer);
 	}
 
 //  public override void _PhysicsProcess(double @delta)
@@ -95,38 +97,52 @@ public abstract partial class Character3D : CharacterBody3D
 //          MoveAndSlide();
 //      }
 //  }
-    public void StartSpeedEffect(float ratio, float duration)
+
+    public void StartSpeedEffect(float @ratio, float @duration)
     {
-        this.CurrentSpeed = this.Speed * ratio;
+        this.CurrentSpeed =
+        this.       Speed *            @ratio     ;
         this.EffectTimer.Timeout += EndSpeedEffect;
-        this.EffectTimer.Start(duration);
+        this.EffectTimer.Start  (                    @duration);
     }
-    public void EndSpeedEffect()
+
+    public void   EndSpeedEffect()
     {
-        this.EffectTimer.Stop();
-        this.CurrentSpeed = this.Speed;
-        this.EffectTimer.WaitTime = 1.0f;
-        this.EffectTimer.Timeout -= EndSpeedEffect;
+        this.EffectTimer.Stop   ();
+        this.CurrentSpeed =
+        this.       Speed ;
+        this.EffectTimer.WaitTime     = 1.0f          ;
+        this.EffectTimer.    Timeout -= EndSpeedEffect;
     }
-    public void BePushed(Vector3 velocity)
+
+    public void BePushed(Vector3 @velocity)
     {
-        this.Velocity = velocity;
+        this.Velocity =          @velocity;
         this.MoveAndSlide();
-        this.Velocity = Vector3.Zero;
+        this.Velocity =  Vector3.Zero     ;
     }
-    public void StartStun(float duration)
+
+    public void StartStun(float @duration)
     {
         this.EffectTimer.Timeout += EndStun;
-        this.EffectTimer.Start(duration);
+        this.EffectTimer.Start( @duration) ;
         this.ProcessMode = ProcessModeEnum.Disabled;
     }
-    public void EndStun()
+
+    public void   EndStun()
     {
         this.EffectTimer.Stop();
         this.EffectTimer.Timeout -= EndStun;
-        this.ProcessMode = ProcessModeEnum.Inherit;
+        this.ProcessMode = ProcessModeEnum
+                        .          Inherit ;
     }
 }
+
+
+
+
+
+
 
 
 

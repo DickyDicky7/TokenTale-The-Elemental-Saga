17c2181f5923d30af67edc0c52afe89134db3c9b
ZeVnan <thienan27103@gmail.com>
2024-06-04 22:04:00 +0700

    Drop manager

---
+++ game_objects/items/DropManager.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+namespace TokenTaleTheElementalSaga;
+public partial class DropManager : Node
+{
+	private static DropManager _instance;
+	public static DropManager GetInstance()
+	{
+		if (_instance == null)
+			_instance = new DropManager();
+		return _instance;
+	}
+	private DropManager()
+	{
+
+	}
+	public List<SoulOrCoin> CoinList { get; set; } = new();
+	public List<ElementalToken> TokenList { get; set; } = new();
+}
